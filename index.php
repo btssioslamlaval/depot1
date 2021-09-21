7042012ad90a9a4afde9eecc6f3000a45b0e4ef0
leprof <ddwlr72@gmail.com>
2021-09-21 13:04:29 +0200

    acceptation du conflit

--- index.php
+++ index.php
@@ -4,9 +4,5 @@
      echo '<tr><td>titre</td><td>description</td></tr>' ;
      echo '<tr><td>titre 1</td><td>description du titre 1</td></tr>' ;
      echo '</table>' ;
-<<<<<<< HEAD
      echo '<br>';
 ?>
-=======
-?>
->>>>>>> 72da59f5f0fa9300e931d8c33eab5a749f4bc0ce
