13d3eb5ddf7efdbcb499ac65bd46d94af5da3292
BTSSIO_LAVAL <mmelerayd@gmail.com>
2021-09-21 13:53:56 +0200

    Update index.php
    cleanningg

--- index.php
+++ index.php
@@ -5,13 +5,5 @@
      echo '<tr><td>titre 1</td><td>description du titre 1</td></tr>' ;
      echo '</table>' ;
      echo '<br>';
-     Oh non!
 ?>
-<<<<<<< HEAD
-ttttt
-=======
-yes i did it!!
-Quoi!
-enfin
-pppp
->>>>>>> a1d8444b70490e23df07ac2c23c8e9d66ac1fcb8
+
