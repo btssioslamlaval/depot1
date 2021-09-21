72da59f5f0fa9300e931d8c33eab5a749f4bc0ce
BTSSIO_LAVAL <mmelerayd@gmail.com>
2021-09-21 12:43:53 +0200

    Update index.php
    modification de la deuxième ligne du tableau

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
      echo 'mon premier test sur git' ;
      echo '<table border=1>' ;
      echo '<tr><td>titre</td><td>description</td></tr>' ;
-     echo '<tr><td>titre</td><td>description</td></tr>' ;
+     echo '<tr><td>titre 1</td><td>description du titre 1</td></tr>' ;
      echo '</table>' ;
 ?>
